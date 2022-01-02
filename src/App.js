c35be0573be21c39124c80c636b6afdb929ad81c
Mohamed Iliyas <37535362+iliyas2008@users.noreply.github.com>
2022-01-03 00:15:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [posts, setPosts] = useState([])
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then((res)=>{
+            console.log(res.data)
+            setPosts(res.data)
+        })
+    }, []);
+
+    function rand() { return Math.floor(Math.random() * 255) }
+    
+    return(
+            <>
+            {   posts.map((item)=>{
+                
+                return (
+                  <>
+                    <div class="col-sm-6">
+                        <div className="card mx-2 my-1 bg-light">
+                          <div key={item.userId+1} 
+                          style={{backgroundColor:`rgb(${rand()}, ${rand()}, ${rand()})`}}
+                          className="card-header text-dark"></div>
+                          <div className="card-body bg-warning rounded thumbnail">
+                            <h5  key={item.id+1+"title"} className="card-title text-danger text-uppercase">
+                              {item.id} . {item.title}
+                            </h5>
+                            <p  key={item.id+1+"body"} className="card-text text-success">{item.body}</p>
+                          </div>
+                        </div>
+                    </div>
+                  </>
+                );
+            })
+            }
+            </>        
+    )
+    
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.Fragment>
+        <h1 className='text-center text-capitalize'>Displaying data from jsonplaceholder using axios</h1>
+        <div className="container">
+            <div className="row">
+                <App />
+            </div>
+        </div>
+    </React.Fragment>,
+    document.getElementById("root")
+)
